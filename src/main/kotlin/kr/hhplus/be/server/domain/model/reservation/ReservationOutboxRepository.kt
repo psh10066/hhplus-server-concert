abc12950f8be213d69bc22527c19abf8f00beacb
이호준 <psh10066@gmail.com>
2025-02-21 06:03:44 +0900

    Feat: Kafka 발행 실패 케이스 재처리 스케줄러 구현

--- src/main/kotlin/kr/hhplus/be/server/domain/model/reservation/ReservationOutboxRepository.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/model/reservation/ReservationOutboxRepository.kt
@@ -1,8 +1,12 @@
 package kr.hhplus.be.server.domain.model.reservation
 
+import java.time.LocalDateTime
+
 interface ReservationOutboxRepository {
 
     fun save(reservationOutbox: ReservationOutbox): ReservationOutbox
 
     fun findByOperationTypeAndOperationId(operationType: String, operationId: String): ReservationOutbox?
+
+    fun findAllByStatusAndCreatedAtBefore(status: ReservationOutboxStatus, localDateTime: LocalDateTime): List<ReservationOutbox>
 }
--- src/main/kotlin/kr/hhplus/be/server/infrastructure/dao/reservation/ReservationOutboxRepositoryImpl.kt
+++ src/main/kotlin/kr/hhplus/be/server/infrastructure/dao/reservation/ReservationOutboxRepositoryImpl.kt
@@ -2,7 +2,9 @@ package kr.hhplus.be.server.infrastructure.dao.reservation
 
 import kr.hhplus.be.server.domain.model.reservation.ReservationOutbox
 import kr.hhplus.be.server.domain.model.reservation.ReservationOutboxRepository
+import kr.hhplus.be.server.domain.model.reservation.ReservationOutboxStatus
 import org.springframework.stereotype.Component
+import java.time.LocalDateTime
 
 @Component
 class ReservationOutboxRepositoryImpl(
@@ -16,5 +18,13 @@ class ReservationOutboxRepositoryImpl(
 
     override fun findByOperationTypeAndOperationId(operationType: String, operationId: String): ReservationOutbox? {
         return reservationOutboxJpaRepository.findByOperationTypeAndOperationId(operationType, operationId)?.toModel()
+    }
+
+    override fun findAllByStatusAndCreatedAtBefore(
+        status: ReservationOutboxStatus,
+        localDateTime: LocalDateTime
+    ): List<ReservationOutbox> {
+        return reservationOutboxJpaRepository.findAllByStatusAndCreatedAtBefore(status, localDateTime)
+            .map { it.toModel() }
     }
 }
