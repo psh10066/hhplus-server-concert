c0bec4834ed8b21ee1724416004d7e8348ceed7d
이호준 <psh10066@gmail.com>
2025-02-22 13:57:16 +0900

    Fix: 데이터 플랫폼 컨슈머 오류 수정
    - Kafka Group ID 분리
    - API Call이 아닌 직접 데이터 사용

--- src/main/kotlin/kr/hhplus/be/server/consumer/ExternalConsumer.kt
+++ src/main/kotlin/kr/hhplus/be/server/consumer/ExternalConsumer.kt
@@ -10,13 +10,8 @@ import org.springframework.stereotype.Component
 class ExternalConsumer {
     private val log = LoggerFactory.getLogger(javaClass)
 
-    @KafkaListener(topics = [CONCERT_RESERVATION_TOPIC], groupId = "reservation-group")
+    @KafkaListener(topics = [CONCERT_RESERVATION_TOPIC], groupId = "external-group")
     fun consumeDataPlatform(reservation: Reservation) {
-        try {
-            Thread.sleep(3000) // 외부 서비스 요청 3초 소요
-            log.info("[데이터 플랫폼] 예약 정보 수신 성공")
-        } catch (e: Exception) {
-            log.error("[데이터 플랫폼] 예약 정보 수신 실패")
-        }
+        log.info("[데이터 플랫폼] 예약 정보 수신 성공 reservationId=[${reservation.id}]")
     }
 }
